a48a2d31fdc991f60799e4161a5a0e7edaa46889
Nakhaei Mohammadreza <mohammadreza.nakhaei@aalto.fi>
2023-10-13 11:55:03 +0300

    residual policy trianer

--- offlinerlkit/policy_trainer/__init__.py
+++ offlinerlkit/policy_trainer/__init__.py
@@ -1,7 +1,8 @@
 from offlinerlkit.policy_trainer.mf_policy_trainer import MFPolicyTrainer
 from offlinerlkit.policy_trainer.mb_policy_trainer import MBPolicyTrainer
-
+from offlinerlkit.policy_trainer.adaptive_policy_trainer import ResidualAgentTrainer
 __all__ = [
     "MFPolicyTrainer",
-    "MBPolicyTrainer"
+    "MBPolicyTrainer", 
+    "ResidualAgentTrainer",
 ]

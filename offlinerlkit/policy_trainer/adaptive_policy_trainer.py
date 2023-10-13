5a80b1548a13dc117969777bec07763c0f0f22a4
Nakhaei Mohammadreza <mohammadreza.nakhaei@aalto.fi>
2023-10-13 11:52:43 +0300

    adaptive residual agents

---
+++ offlinerlkit/policy_trainer/adaptive_policy_trainer.py
@@ -0,0 +1,187 @@
+import gym
+import numpy as np
+import torch
+from tqdm import tqdm
+
+from offlinerlkit.dynamics import BaseDynamics
+from offlinerlkit.utils.logger import Logger
+from offlinerlkit.policy import BasePolicy, SACPolicy, TD3Policy
+from offlinerlkit.buffer import ReplayBuffer
+
+
+class ResidualAgentTrainer:
+    def __init__(
+            self, 
+            env: gym.Env,
+            policy: BasePolicy, 
+            dynamics: BaseDynamics, 
+            residual_agent: (SACPolicy, TD3Policy), 
+            real_buffer: ReplayBuffer, 
+            buffer: ReplayBuffer,
+            logger: Logger,
+            res_action_coef:float = 1,
+    ):
+        self.env = env
+        self.policy = policy
+        self.dynamics = dynamics
+        self.agent = residual_agent
+        self.buffer = buffer
+        self.real_buffer = real_buffer
+        self.logger = logger
+        self.res_action_coef = res_action_coef
+
+    def pre_train(self, num_epoch:int, step_per_epoch: int, batch_size:int=256):
+        self.agent.train()
+        num_timesteps = 0
+        self.logger.log('Pre training the agent')
+        for epoch in range(1,num_epoch+1):
+            pbar = tqdm(range(step_per_epoch), desc=f"Epoch #{epoch}/{num_epoch}")
+            for it in pbar:
+                batch = self.real_buffer.sample(batch_size)
+                # augment state
+                obs = batch['observations']
+                next_obs = batch['next_observations']
+                with torch.no_grad():
+                    act, _ = self.policy.actforward(obs, True)
+                    next_act, _ = self.policy.actforward(next_obs, True)
+                batch['observations'] = torch.cat(
+                    [obs, obs.clone(), act], 
+                    dim=-1)
+                batch['next_observations'] = torch.cat(
+                    [next_obs, next_obs.clone(), next_act], 
+                    dim=-1)
+                batch['actions'] = torch.zeros_like(batch['actions'])
+                loss = self.agent.learn(batch)
+                pbar.set_postfix(**loss)
+                for k, v in loss.items():
+                    self.logger.logkv_mean(k, v)
+                num_timesteps+=1
+            self.logger.set_timestep(num_timesteps)
+            self.logger.dumpkvs()
+        return num_timesteps
+    
+    def _prepare_state(self, obs, x_hat, policy_act, k=None):
+        return np.concatenate([obs, x_hat, policy_act], -1)
+    
+    def _prepare_action(self, policy_action, res_action, residual_agent=True):
+        if residual_agent:
+            return np.clip(policy_action + self.res_action_coef*res_action, -1, 1)
+        else:
+            return policy_action
+                
+    @torch.no_grad()
+    def collect_one_episode(self, k, deterministic=False, ts=0.05, residual_agent=True):
+        device = self.real_buffer.device
+        obs, done = self.env.reset(), False
+        x_hat = obs # estimated state
+        ep_reward = 0
+        while not done:
+            policy_act = self.policy.select_action(torch.as_tensor(obs).to(device), True) # policy action
+            state = self._prepare_state(obs, x_hat, policy_act, k)
+            res_action = self.agent.select_action(torch.as_tensor(state).to(device), deterministic) 
+            action = self._prepare_action(policy_act, res_action, residual_agent)            
+            next_obs, reward, done, info = self.env.step(action)
+            pred_next_obs, *_ = self.dynamics.step(obs.reshape(1,-1), action.reshape(1,-1))
+            pred_next_obs = pred_next_obs.reshape(-1,)
+            x_hat_dot = pred_next_obs+k*(obs-x_hat)
+            next_x_hat = x_hat + x_hat_dot*ts
+            next_policy_action = self.policy.select_action(torch.as_tensor(next_obs).to(device), True)
+            next_state = self._prepare_state(next_obs, next_x_hat, next_policy_action, k)
+            self.buffer.add(state, next_state, res_action, reward, done)
+            ep_reward+=reward
+            obs = next_obs
+            x_hat = next_x_hat
+        ep_reward = self.env.get_normalized_score(ep_reward)*100
+        self.logger.logkv('reward', ep_reward)
+        return ep_reward
+    
+    @torch.no_grad()
+    def evaluate(self, k, res_agent=True, deterministic=True, ts=0.05, num_eval=10):
+        device = self.real_buffer.device
+        rewards = np.zeros(num_eval)
+        for n in range(num_eval):
+            obs, done = self.env.reset(), False
+            x_hat = obs # estimated state
+            ep_reward = 0
+            while not done:
+                policy_act = self.policy.select_action(torch.as_tensor(obs).to(device), True) # policy action
+                state = self._prepare_state(obs, x_hat, policy_act, k)
+                res_action = self.agent.select_action(torch.as_tensor(state).to(device), deterministic)
+                action = self._prepare_action(policy_act, res_action, res_agent) 
+                next_obs, reward, done, info = self.env.step(action)
+                pred_next_obs, *_ = self.dynamics.step(obs.reshape(1,-1), action.reshape(1,-1))
+                pred_next_obs = pred_next_obs.reshape(-1,)
+                x_hat_dot = pred_next_obs+k*(obs-x_hat)
+                next_x_hat = x_hat + x_hat_dot*ts
+                ep_reward+=reward
+                obs = next_obs
+                x_hat = next_x_hat
+            rewards[n] = ep_reward
+        ep_reward = self.env.get_normalized_score(np.mean(rewards))*100
+        self.logger.logkv('eval_reward', ep_reward)
+        return ep_reward         
+    
+    def train_epoch(self, num_step:int, batch_size:int=256):
+        self.agent.train()
+        pbar = tqdm(range(num_step),)
+        for i in pbar:
+            batch = self.buffer.sample(batch_size)
+            loss = self.agent.learn(batch)
+            for k, v in loss.items():
+                self.logger.logkv_mean(k, v)
+        self.logger.dumpkvs()
+    
+    def train_sample(self, num_step:int, batch_size:int=256):
+        self.agent.train()
+        for i in range(num_step):
+            batch = self.buffer.sample(batch_size)
+            loss = self.agent.learn(batch)
+        return loss
+        
+
+    def train(self, num_epoch:int, num_train:int, batch_size:int=256):
+        policy_reward= self.evaluate(20, res_agent=False)
+        self.logger.log('Training the agent')
+        self.logger.log(f'Initial policy reward: {policy_reward:.2f}')
+        for e in range(1,num_epoch+1):
+            self.logger.set_timestep(e)
+            reward = self.collect_one_episode(k=20, deterministic=False)
+            self.train_epoch(num_train, batch_size)
+            self.evaluate(k=20)
+
+    def train_continuous(self, max_steps:int, update_ratio: int=2, batch_size:int=256, k:float=20, ts:float=0.05):
+        device = self.real_buffer.device
+        policy_reward= self.evaluate(k, res_agent=False)
+        self.logger.log('Training the agent')
+        self.logger.log(f'Initial policy reward: {policy_reward:.2f}')
+        total_t = 0
+        while True:
+            obs, done = self.env.reset(), False
+            x_hat = obs # estimated state
+            ep_reward = 0
+            while not done and total_t<=max_steps:
+                total_t+=1
+                policy_act = self.policy.select_action(torch.as_tensor(obs).to(device), True) # policy action
+                state = self._prepare_state(obs, x_hat, policy_act, k)
+                res_action = self.agent.select_action(torch.as_tensor(state).to(device), False)
+                action = self._prepare_action(policy_act, res_action, True) 
+                next_obs, reward, done, info = self.env.step(action)
+                pred_next_obs, *_ = self.dynamics.step(obs.reshape(1,-1), action.reshape(1,-1))
+                pred_next_obs = pred_next_obs.reshape(-1,)
+                x_hat_dot = pred_next_obs+k*(obs-x_hat)
+                next_x_hat = x_hat + x_hat_dot*ts
+                next_policy_action = self.policy.select_action(torch.as_tensor(next_obs).to(device), True)
+                next_state = self._prepare_state(next_obs, next_x_hat, next_policy_action, k)
+                self.buffer.add(state, next_state, res_action, reward, done)
+                ep_reward+=reward
+                obs = next_obs
+                x_hat = next_x_hat 
+                # update the policy
+                loss = self.train_sample(update_ratio, batch_size)
+                for key,val in loss.items():
+                    self.logger.logkv_mean(key, val)
+            ep_reward = self.env.get_normalized_score(ep_reward)*100
+            self.logger.logkv('reward', ep_reward)      
+            self.evaluate(k, res_agent=True) 
+            self.logger.set_timestep(total_t) 
+            self.logger.dumpkvs()                   
---
+++ run_example/adaptive_combo_td3.py
@@ -0,0 +1,205 @@
+import argparse
+import os
+import random
+import gym
+import numpy as np
+import torch
+import d4rl 
+
+from offlinerlkit.nets import MLP
+from offlinerlkit.modules import Actor, ActorProb, Critic, TanhDiagGaussian, EnsembleDynamicsModel
+from offlinerlkit.dynamics import EnsembleDynamics
+from offlinerlkit.utils.scaler import StandardScaler
+from offlinerlkit.utils.termination_fns import get_termination_fn
+from offlinerlkit.utils.logger import make_log_dirs, load_args
+from offlinerlkit.policy import COMBOPolicy, TD3BCPolicy
+from offlinerlkit.buffer import ReplayBuffer
+from offlinerlkit.utils.logger import Logger
+from offlinerlkit.utils.load_dataset import qlearning_dataset
+from offlinerlkit.policy_trainer import ResidualAgentTrainer
+from offlinerlkit.utils.noise import GaussianNoise
+from offlinerlkit.utils.modify_env import SimpleModifiedENV, SemiSimpleModifiedENV, ModifiedENV
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--algo-name", type=str, default="combo")
+    parser.add_argument("--task", type=str, default="hopper-medium-v2")
+    parser.add_argument("--max-delta", type=float, default=0.2, help='maximum perturbation in mass')
+    parser.add_argument("--env-mode", type=str, default="complex", 
+                        help='simple, semi-simple or complex environment, different types of perturbation')
+    parser.add_argument("--eps", type=float, default=0.1, help='exploration noise for residual agent')
+    parser.add_argument("--coeff-residual", type=float, default=1)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--max-steps", type=int, default=int(2e5))
+    parser.add_argument('--n-update', type=int, default=1, help='number of updates in each samples')
+    parser.add_argument("--actor-lr", type=float, default=1e-4)
+    parser.add_argument("--critic-lr", type=float, default=3e-4)
+    parser.add_argument("--hidden-dims", type=int, nargs='*', default=[256, 256])
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--tau", type=float, default=0.005)
+    parser.add_argument("--buffer-size", type=int, default=100000)
+    parser.add_argument("--tag", type=str, default='')
+    return parser.parse_args()
+
+
+def train(mainargs=get_args(), delta_m=1):
+    load_path = f'log/{mainargs.task}/{mainargs.algo_name}/seed_{mainargs.seed}'
+    args = load_args(os.path.join(load_path, 'record/hyper_param.json'))
+    args.action_dim = int(args.action_dim) # when loaded from json file, default is float
+    # create env and dataset
+    env = gym.make(args.task)
+    dataset = qlearning_dataset(env)
+    EnvMode = {
+        'simple': SimpleModifiedENV,
+        'semi-simple': SemiSimpleModifiedENV,
+        'complex': ModifiedENV,
+    }
+    env = EnvMode[mainargs.env_mode](env, mainargs.max_delta)
+
+    # seed
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    torch.backends.cudnn.deterministic = True
+    env.seed(args.seed)
+
+    # create policy model
+    actor_backbone = MLP(input_dim=np.prod(args.obs_shape), hidden_dims=args.hidden_dims)
+    critic1_backbone = MLP(input_dim=np.prod(args.obs_shape) + args.action_dim, hidden_dims=args.hidden_dims)
+    critic2_backbone = MLP(input_dim=np.prod(args.obs_shape) + args.action_dim, hidden_dims=args.hidden_dims)
+    dist = TanhDiagGaussian(
+        latent_dim=getattr(actor_backbone, "output_dim"),
+        output_dim=args.action_dim,
+        unbounded=True,
+        conditioned_sigma=True
+    )
+    actor = ActorProb(actor_backbone, dist, args.device)
+    critic1 = Critic(critic1_backbone, args.device)
+    critic2 = Critic(critic2_backbone, args.device)
+    actor_optim = None
+    critic1_optim = None
+    critic2_optim = None
+    alpha = None
+
+    # create dynamics
+    dynamics_model = EnsembleDynamicsModel(
+        obs_dim=np.prod(args.obs_shape),
+        action_dim=args.action_dim,
+        hidden_dims=args.dynamics_hidden_dims,
+        num_ensemble=args.n_ensemble,
+        num_elites=args.n_elites,
+        weight_decays=args.dynamics_weight_decay,
+        device=args.device
+    )
+    dynamics_optim = None
+    scaler = StandardScaler()
+    scaler.load_scaler(f'{load_path}/model')
+    termination_fn = get_termination_fn(task=args.task)
+    dynamics = EnsembleDynamics(
+        dynamics_model,
+        dynamics_optim,
+        scaler,
+        termination_fn
+    )
+    dynamics.load(f'{load_path}/model')
+
+    # create policy
+    policy = COMBOPolicy(
+        dynamics,
+        actor,
+        critic1,
+        critic2,
+        actor_optim,
+        critic1_optim,
+        critic2_optim,
+        action_space=env.action_space,
+        tau=args.tau,
+        gamma=args.gamma,
+        alpha=alpha,
+        cql_weight=args.cql_weight,
+        temperature=args.temperature,
+        max_q_backup=args.max_q_backup,
+        deterministic_backup=args.deterministic_backup,
+        with_lagrange=args.with_lagrange,
+        lagrange_threshold=args.lagrange_threshold,
+        cql_alpha_lr=args.cql_alpha_lr,
+        num_repeart_actions=args.num_repeat_actions,
+        uniform_rollout=args.uniform_rollout,
+        rho_s=args.rho_s
+    )
+    
+    policy.load_state_dict(torch.load(f'{load_path}/checkpoint/policy.pth'))
+
+    res_actor_backbone = MLP(input_dim=2*np.prod(args.obs_shape)+args.action_dim, hidden_dims=mainargs.hidden_dims)
+    res_critic1_backbone = MLP(input_dim=2*np.prod(args.obs_shape)+2*args.action_dim, hidden_dims=mainargs.hidden_dims)
+    res_critic2_backbone = MLP(input_dim=2*np.prod(args.obs_shape)+2*args.action_dim, hidden_dims=mainargs.hidden_dims)
+
+    res_actor = Actor(res_actor_backbone, args.action_dim, device=args.device)
+    res_critic1 = Critic(res_critic1_backbone, args.device)
+    res_critic2 = Critic(res_critic2_backbone, args.device)
+    res_actor_optim = torch.optim.Adam(res_actor.parameters(), lr=mainargs.actor_lr)
+    res_critic1_optim = torch.optim.Adam(res_critic1.parameters(), lr=mainargs.critic_lr)
+    res_critic2_optim = torch.optim.Adam(res_critic2.parameters(), lr=mainargs.critic_lr)   
+
+    noise = GaussianNoise(0, mainargs.eps)
+
+    res_policy = TD3BCPolicy(
+        res_actor,
+        res_critic1,
+        res_critic2,
+        res_actor_optim,
+        res_critic1_optim,
+        res_critic2_optim,
+        tau=mainargs.tau,
+        gamma=mainargs.gamma,
+        exploration_noise=noise,
+        policy_noise=0.2,
+    )
+
+    real_buffer = ReplayBuffer(
+        buffer_size=len(dataset["observations"]),
+        obs_shape=tuple(args.obs_shape),
+        obs_dtype=np.float32,
+        action_dim=args.action_dim,
+        action_dtype=np.float32,
+        device=args.device
+    )
+    real_buffer.load_dataset(dataset)
+    res_obs_shape = (args.obs_shape[0]*2+args.action_dim, )
+    aug_buffer = ReplayBuffer(
+        buffer_size=mainargs.buffer_size,
+        obs_shape=res_obs_shape,
+        obs_dtype=np.float32,
+        action_dim=args.action_dim,
+        action_dtype=np.float32,
+        device=args.device
+    )
+
+    # log
+    log_dirs = make_log_dirs(f'{args.task}-adaptive', f'{args.algo_name}_td3', args.seed, vars(mainargs), record_params=['tag'])
+    # key: output file name, value: output handler type
+    output_config = {
+        "consoleout_backup": "stdout",
+        "policy_training_progress": "csv",
+        "dynamics_training_progress": "csv",
+        "tb": "tensorboard"
+    }
+    logger = Logger(log_dirs, output_config)
+    logger.log_hyperparameters(vars(args))
+
+    res_agent = ResidualAgentTrainer(
+        env, policy, dynamics, res_policy, 
+        real_buffer, aug_buffer, logger, 
+        res_action_coef=mainargs.coeff_residual,
+        )    
+    res_agent.train_continuous(mainargs.max_steps, mainargs.n_update, args.batch_size,)
+    torch.save(res_policy.state_dict(), f'{log_dirs}/model/residual_agent.pth')
+
+
+
+if __name__ == '__main__':
+    
+    mainargs=get_args()
+    train(mainargs)

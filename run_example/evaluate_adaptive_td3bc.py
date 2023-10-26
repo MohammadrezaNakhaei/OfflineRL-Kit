fb0486dac6386e1d4ef91f7daed5d3ab23672e2a
Nakhaei Mohammadreza <mohammadreza.nakhaei@aalto.fi>
2023-10-26 14:35:25 +0300

    add evaluation scripts

---
+++ run_example/evaluate_adaptive_td3bc.py
@@ -0,0 +1,190 @@
+import argparse
+import os
+import random
+import gym
+import numpy as np
+import torch
+import d4rl 
+
+from offlinerlkit.nets import MLP
+from offlinerlkit.modules import Actor, ActorProb, Critic, TanhDiagGaussian, EnsembleDynamicsModel
+from offlinerlkit.dynamics import EnsembleDynamics
+from offlinerlkit.utils.scaler import StandardScaler
+from offlinerlkit.utils.termination_fns import get_termination_fn
+from offlinerlkit.utils.logger import make_log_dirs, load_args
+from offlinerlkit.policy import COMBOPolicy, TD3BCPolicy
+from offlinerlkit.buffer import ReplayBuffer
+from offlinerlkit.utils.logger import Logger
+from offlinerlkit.utils.load_dataset import qlearning_dataset
+from offlinerlkit.policy_trainer import ResidualAgentTrainer
+from offlinerlkit.utils.noise import GaussianNoise
+from offlinerlkit.utils.modify_env import SimpleModifiedENV, SemiSimpleModifiedENV, ModifiedENV
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--algo-name", type=str, default="combo")
+    parser.add_argument("--task", type=str, default="hopper-medium-v2")
+    parser.add_argument("--max-delta", type=float, default=0.2, help='maximum perturbation in mass')
+    parser.add_argument("--env-mode", type=str, default="complex", 
+                        help='simple, semi-simple or complex environment, different types of perturbation')
+    parser.add_argument("--eps", type=float, default=0.1, help='exploration noise for residual agent')
+    parser.add_argument("--coeff-residual", type=float, default=1)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--offline-seed", type=int, default=40, help='seed of loaded offline agent')
+    parser.add_argument("--max-steps", type=int, default=int(5e5))
+    parser.add_argument('--n-update', type=int, default=1, help='number of updates in each samples')
+    parser.add_argument("--actor-lr", type=float, default=1e-4)
+    parser.add_argument("--critic-lr", type=float, default=3e-4)
+    parser.add_argument("--hidden-dims", type=int, nargs='*', default=[256, 256])
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--tau", type=float, default=0.005)
+    parser.add_argument("--buffer-size", type=int, default=100000)
+    parser.add_argument("--tag", type=str, default='')
+    return parser.parse_args()
+
+
+def train(mainargs=get_args()):
+    load_path = f'log/{mainargs.task}/{mainargs.algo_name}/seed_{mainargs.offline_seed}'
+    args = load_args(os.path.join(load_path, 'record/hyper_param.json'))
+    args.action_dim = int(args.action_dim) # when loaded from json file, default is float
+    # create env and dataset
+    env = gym.make(args.task)
+    eval_env = gym.make(args.task)
+    EnvMode = {
+        'simple': SimpleModifiedENV,
+        'semi-simple': SemiSimpleModifiedENV,
+        'complex': ModifiedENV,
+    }
+    env = EnvMode[mainargs.env_mode](env, mainargs.max_delta)
+    eval_env = ModifiedENV(eval_env, mainargs.max_delta)
+    # seed
+    random.seed(mainargs.seed)
+    np.random.seed(mainargs.seed)
+    torch.manual_seed(mainargs.seed)
+    torch.cuda.manual_seed_all(mainargs.seed)
+    torch.backends.cudnn.deterministic = True
+    env.seed(mainargs.seed)
+    eval_env.seed(mainargs.seed)
+
+    # create policy model
+    actor_backbone = MLP(input_dim=np.prod(args.obs_shape), hidden_dims=args.hidden_dims)
+    critic1_backbone = MLP(input_dim=np.prod(args.obs_shape) + args.action_dim, hidden_dims=args.hidden_dims)
+    critic2_backbone = MLP(input_dim=np.prod(args.obs_shape) + args.action_dim, hidden_dims=args.hidden_dims)
+    dist = TanhDiagGaussian(
+        latent_dim=getattr(actor_backbone, "output_dim"),
+        output_dim=args.action_dim,
+        unbounded=True,
+        conditioned_sigma=True
+    )
+    actor = ActorProb(actor_backbone, dist, args.device)
+    critic1 = Critic(critic1_backbone, args.device)
+    critic2 = Critic(critic2_backbone, args.device)
+    actor_optim = None
+    critic1_optim = None
+    critic2_optim = None
+    alpha = None
+
+    # create dynamics
+    dynamics_model = EnsembleDynamicsModel(
+        obs_dim=np.prod(args.obs_shape),
+        action_dim=args.action_dim,
+        hidden_dims=args.dynamics_hidden_dims,
+        num_ensemble=args.n_ensemble,
+        num_elites=args.n_elites,
+        weight_decays=args.dynamics_weight_decay,
+        device=args.device
+    )
+    dynamics_optim = None
+    scaler = StandardScaler()
+    scaler.load_scaler(f'{load_path}/model')
+    termination_fn = get_termination_fn(task=args.task)
+    dynamics = EnsembleDynamics(
+        dynamics_model,
+        dynamics_optim,
+        scaler,
+        termination_fn
+    )
+    dynamics.load(f'{load_path}/model')
+
+    # create policy
+    policy = COMBOPolicy(
+        dynamics,
+        actor,
+        critic1,
+        critic2,
+        actor_optim,
+        critic1_optim,
+        critic2_optim,
+        action_space=env.action_space,
+        tau=args.tau,
+        gamma=args.gamma,
+        alpha=alpha,
+        cql_weight=args.cql_weight,
+        temperature=args.temperature,
+        max_q_backup=args.max_q_backup,
+        deterministic_backup=args.deterministic_backup,
+        with_lagrange=args.with_lagrange,
+        lagrange_threshold=args.lagrange_threshold,
+        cql_alpha_lr=args.cql_alpha_lr,
+        num_repeart_actions=args.num_repeat_actions,
+        uniform_rollout=args.uniform_rollout,
+        rho_s=args.rho_s
+    )
+    
+    policy.load_state_dict(torch.load(f'{load_path}/checkpoint/policy.pth'))
+
+    res_actor_backbone = MLP(input_dim=2*np.prod(args.obs_shape)+args.action_dim, hidden_dims=mainargs.hidden_dims)
+    res_critic1_backbone = MLP(input_dim=2*np.prod(args.obs_shape)+2*args.action_dim, hidden_dims=mainargs.hidden_dims)
+    res_critic2_backbone = MLP(input_dim=2*np.prod(args.obs_shape)+2*args.action_dim, hidden_dims=mainargs.hidden_dims)
+
+    res_actor = Actor(res_actor_backbone, args.action_dim, device=args.device)
+    res_critic1 = Critic(res_critic1_backbone, args.device)
+    res_critic2 = Critic(res_critic2_backbone, args.device)
+    res_actor_optim = None
+    res_critic1_optim = None
+    res_critic2_optim = None
+
+    noise = GaussianNoise(0, mainargs.eps)
+
+    res_policy = TD3BCPolicy(
+        res_actor,
+        res_critic1,
+        res_critic2,
+        res_actor_optim,
+        res_critic1_optim,
+        res_critic2_optim,
+        tau=mainargs.tau,
+        gamma=mainargs.gamma,
+        exploration_noise=noise,
+        policy_noise=0.2,
+    )
+
+    real_buffer = None
+    aug_buffer = None
+
+    # log
+    log_dirs = make_log_dirs(f'{args.task}-adaptive', f'{args.algo_name}_td3', mainargs.seed, vars(mainargs), record_params=['tag'])
+    # key: output file name, value: output handler type
+    logger = None
+    res_policy.state_dict(torch.load(f'{log_dirs}/model/residual_agent_best.pth'))
+    res_agent = ResidualAgentTrainer(
+        env, eval_env, policy, dynamics, res_policy, 
+        real_buffer, aug_buffer, logger, 
+        res_action_coef=mainargs.coeff_residual,
+        )    
+        
+    reward_offline, std_offline = res_agent.evaluate(res_agent=False)
+    reward_residual, std_residual = res_agent.evaluate(res_agent=True)
+
+    print('\nOffline Policy')
+    print(f'avg reward: {reward_offline} \t std: {std_offline}')
+    print('\nResidual Policy')
+    print(f'avg reward: {reward_residual} \t std: {std_residual}')
+
+
+
+if __name__ == '__main__':
+    
+    mainargs=get_args()
+    train(mainargs)

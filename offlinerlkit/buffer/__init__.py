a47d4a16ee13cd337ea6c416b83f9aa1ce796452
Nakhaei Mohammadreza <mohammadreza.nakhaei@aalto.fi>
2023-11-01 11:13:28 +0200

    add buffer for N point sequence, used for adaptive encoder

--- offlinerlkit/buffer/__init__.py
+++ offlinerlkit/buffer/__init__.py
@@ -1,6 +1,7 @@
 from offlinerlkit.buffer.buffer import ReplayBuffer
-
+from offlinerlkit.buffer.seq_npoint_buffer import NSequenceBuffer
 
 __all__ = [
-    "ReplayBuffer"
+    "ReplayBuffer",
+    "NSequenceBuffer",
 ]
